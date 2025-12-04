8b26ee4580fd2f3a7a39c8a5962b5b54bc2f981c
Prathamesh Patil <patilprathamesh76@gmail.com>
2025-12-04 19:30:33 +0100

    first commit

---
+++ tests/example1.spec.js
@@ -0,0 +1,30 @@
+import test, { expect } from "@playwright/test";
+
+
+
+test.beforeAll('---Test Start---',()=>{
+    console.log('---Test Start---')
+})
+
+test('navigate',async ({page}) => {
+    await page.goto('http://127.0.0.1:5500/res/testsite.html');
+
+    await expect(page).toHaveTitle('Test Automation Practice Site')    
+})
+
+
+test('Invalid Login',async ({page}) => {
+    await page.goto('http://127.0.0.1:5500/res/testsite.html');
+
+    await expect(page.getByRole("checkbox",{name: 'Remember me'})).toBeChecked({checked:false});
+    await page.getByRole('checkbox', { name: 'Remember me' }).check();
+    await expect(page.getByRole("checkbox",{name: 'Remember me'})).toBeChecked();
+
+    await page.getByRole('button',{name: 'Login',exact:true}).click();
+    const error = page.locator('p[id="login-message"]');
+    await expect(error).toHaveText('Invalid credentials.')
+})
+
+test.afterAll('---Test End---',()=>{
+    console.log('---Test End---')
+})
